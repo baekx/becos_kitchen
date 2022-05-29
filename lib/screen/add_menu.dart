8b6d38bfd8e19ccdea7d25696032f82cfd7a04fa
seo <seungbaek.seo@spacemarket.co.jp>
2022-05-29 18:10:14 +0900

    TextField追加

--- lib/screen/add_menu.dart
+++ lib/screen/add_menu.dart
@@ -11,9 +11,26 @@ class AddMenuScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Text("画像"),
-          Text("タイトル"),
-          Text("作成日"),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 40.0,
+              top: 80.0,
+              right: 40.0,
+            ),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide: BorderSide(color: Colors.green, width: 2.0)),
+                  labelStyle: TextStyle(fontSize: 12, color: Colors.green[300]),
+                  labelText: "Menu Title",
+                  floatingLabelStyle: TextStyle(fontSize: 12),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide:
+                          BorderSide(color: Colors.green[100]!, width: 1.0))),
+            ),
+          )
         ],
       ),
     );
