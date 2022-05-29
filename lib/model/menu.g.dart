1427cb1335efd52fde4c2ed4f2a64cbbf6e2d858
seo <seungbaek.seo@spacemarket.co.jp>
2022-05-29 16:34:26 +0900

    Menu Data class作成

---
+++ lib/model/menu.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'menu.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Menu _$$_MenuFromJson(Map<String, dynamic> json) => _$_Menu(
+      title: json['title'] as String,
+      score: json['score'] as int,
+    );
+
+Map<String, dynamic> _$$_MenuToJson(_$_Menu instance) => <String, dynamic>{
+      'title': instance.title,
+      'score': instance.score,
+    };
