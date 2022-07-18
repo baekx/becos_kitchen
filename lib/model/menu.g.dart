196c42fbd79ed11a231bee3d24f048823b48a90e
seo <seungbaek.seo@spacemarket.co.jp>
2022-07-18 18:58:18 +0900

    Menuモデル作成

--- lib/model/menu.g.dart
+++ lib/model/menu.g.dart
@@ -1,18 +1,21 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'menu_state.dart';
+part of 'menu.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
-_$_MenuState _$$_MenuStateFromJson(Map<String, dynamic> json) => _$_MenuState(
-      title: json['title'] as String,
-      score: json['score'] as int,
+_$_Menu _$$_MenuFromJson(Map<String, dynamic> json) => _$_Menu(
+      name: json['name'] as String,
+      rate: json['rate'] as int,
+      image: json['image'] as String,
+      tag: (json['tag'] as List<dynamic>).map((e) => e as String).toList(),
     );
 
-Map<String, dynamic> _$$_MenuStateToJson(_$_MenuState instance) =>
-    <String, dynamic>{
-      'title': instance.title,
-      'score': instance.score,
+Map<String, dynamic> _$$_MenuToJson(_$_Menu instance) => <String, dynamic>{
+      'name': instance.name,
+      'rate': instance.rate,
+      'image': instance.image,
+      'tag': instance.tag,
     };
--- lib/repository/menu_dao.dart
+++ lib/repository/menu_dao.dart
@@ -1,27 +1,29 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-import '../model/menu_state.dart';
+import '../model/menu.dart';
 
 class MenuDao {
   static final CollectionReference _menus =
       FirebaseFirestore.instance.collection('menu');
 
-  static void setMenu(MenuState newMenu) {
-    try {
-      final document = <String, dynamic>{
-        'title': newMenu.title,
-        'score': newMenu.score
-      };
-      _menus.doc().set(document);
-      print('メニュー追加完了');
-      // return true;
-    } on FirebaseException catch (e) {
-      print('メニュー追加失敗: $e');
-      // return false;
-    }
+  static void getMenuList() {
+    // try {
+    //
+    // }
   }
 
-  // void setMenu(MenuState menu) {
-  //   _menus.add(menu.toJson());
-  // }
+  static void setMenu(Menu newMenu) {
+    // try {
+    //   final document = <String, dynamic>{
+    //     'title': newMenu.title,
+    //     'score': newMenu.score
+    //   };
+    //   _menus.doc().set(document);
+    //   print('メニュー追加完了');
+    //   // return true;
+    // } on FirebaseException catch (e) {
+    //   print('メニュー追加失敗: $e');
+    //   // return false;
+    // }
+  }
 }
