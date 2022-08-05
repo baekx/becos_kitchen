9d70c395da3e6befca9b0168b20ffa4c3c4ac756
seo <seungbaek.seo@spacemarket.co.jp>
2022-08-05 20:07:02 +0900

    URLフォーム作成

--- lib/screen/add_menu/add_menu_first_page.dart
+++ lib/screen/add_menu/add_menu_first_page.dart
@@ -21,56 +21,59 @@ class AddMenuFirstPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final _vm = ref.watch(addMenuPageProvider);
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "料理を追加",
-          style: TextStyle(color: Color(green)),
-        ),
-        automaticallyImplyLeading: false,
-        centerTitle: true,
-        actions: [
-          IconButton(
-            icon: SvgPicture.asset('assets/icons/cancel.svg'),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          )
-        ],
-      ),
-      resizeToAvoidBottomInset: false,
-      body: Padding(
-        padding: const EdgeInsets.only(left: 24.0, right: 24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            // 写真
-            const ColumnPadding(height: 16.0),
-            const Label(text: "写真"),
-            MenuImage(imagePath: image),
-            // タイトル
-            const ColumnPadding(height: 16.0),
-            const Label(text: "タイトル"),
-            const MenuTitle(),
-            // タグ
-            const ColumnPadding(height: 8.0),
-            const Label(text: "タグ"),
-            const MenuTagList(),
-            // 次へボタン
-            const ColumnPadding(height: 8.0),
-            ButtonExpanded(
-              text: "次へ",
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "料理を追加",
+            style: TextStyle(color: Color(green)),
+          ),
+          automaticallyImplyLeading: false,
+          centerTitle: true,
+          actions: [
+            IconButton(
+              icon: SvgPicture.asset('assets/icons/cancel.svg'),
               onPressed: () {
-                // ref.watch(addMenuPageProvider.notifier).addMenu();
-                Navigator.of(context).push(CupertinoPageRoute(
-                  builder: (context) => AddMenuSecondPage(),
-                ));
+                Navigator.of(context).pop();
               },
-            ),
-            const ColumnPadding(height: 32.0),
+            )
           ],
+        ),
+        resizeToAvoidBottomInset: false,
+        body: Padding(
+          padding: const EdgeInsets.only(left: 24.0, right: 24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              // 写真
+              const ColumnPadding(height: 16.0),
+              const Label(text: "写真"),
+              MenuImage(imagePath: image),
+              // タイトル
+              const ColumnPadding(height: 16.0),
+              const Label(text: "タイトル"),
+              const MenuTitle(),
+              // タグ
+              const ColumnPadding(height: 8.0),
+              const Label(text: "タグ"),
+              const MenuTagList(),
+              // 次へボタン
+              const ColumnPadding(height: 8.0),
+              ButtonExpanded(
+                text: "次へ",
+                onPressed: () {
+                  // ref.watch(addMenuPageProvider.notifier).addMenu();
+                  Navigator.of(context).push(CupertinoPageRoute(
+                    builder: (context) => AddMenuSecondPage(),
+                  ));
+                },
+              ),
+              const ColumnPadding(height: 32.0),
+            ],
+          ),
         ),
       ),
     );
--- lib/screen/add_menu/add_menu_second_page.dart
+++ lib/screen/add_menu/add_menu_second_page.dart
@@ -2,6 +2,7 @@ import 'package:becos_kitchen/common/material_color.dart';
 import 'package:becos_kitchen/screen/add_menu/label.dart';
 import 'package:becos_kitchen/screen/add_menu/menu_date_picker.dart';
 import 'package:becos_kitchen/screen/add_menu/menu_rate.dart';
+import 'package:becos_kitchen/screen/add_menu/menu_url.dart';
 import 'package:becos_kitchen/screen/common/column_padding.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -12,39 +13,46 @@ class AddMenuSecondPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "料理を追加",
-          style: TextStyle(color: Color(green)),
-        ),
-        automaticallyImplyLeading: false,
-        centerTitle: true,
-        actions: [
-          IconButton(
-              icon: SvgPicture.asset('assets/icons/cancel.svg'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              })
-        ],
-      ),
-      resizeToAvoidBottomInset: false,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            // 星
-            const ColumnPadding(height: 24.0),
-            const Label(text: "お気に入り"),
-            const MenuRate(),
-            // 料理した日
-            const ColumnPadding(height: 30.0),
-            const Label(text: "料理した日"),
-            MenuDatePicker()
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "料理を追加",
+            style: TextStyle(color: Color(green)),
+          ),
+          automaticallyImplyLeading: false,
+          centerTitle: true,
+          actions: [
+            IconButton(
+                icon: SvgPicture.asset('assets/icons/cancel.svg'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                })
           ],
+        ),
+        resizeToAvoidBottomInset: false,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            children: const [
+              // 星
+              ColumnPadding(height: 24.0),
+              Label(text: "お気に入り"),
+              MenuRate(),
+              // 料理した日
+              ColumnPadding(height: 30.0),
+              Label(text: "料理した日"),
+              MenuDatePicker(),
+              // URL
+              ColumnPadding(height: 30.0),
+              Label(text: "URL"),
+              MenuUrl(),
+            ],
+          ),
         ),
       ),
     );
