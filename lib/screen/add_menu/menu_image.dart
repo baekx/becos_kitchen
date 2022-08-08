537e11a3af964cfbcca5df5111a6192b928bd05d
seo <seungbaek.seo@spacemarket.co.jp>
2022-08-08 16:02:01 +0900

    addMenu画像がないときのデフォルト画像追加

--- lib/screen/add_menu/menu_image.dart
+++ lib/screen/add_menu/menu_image.dart
@@ -1,3 +1,5 @@
+import 'package:becos_kitchen/common/material_color.dart';
+import 'package:becos_kitchen/screen/common/column_padding.dart';
 import 'package:becos_kitchen/view_model/add_menu_page_vm.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -10,29 +12,46 @@ class MenuImage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final state = ref.watch(addMenuViewModelProvider);
     final vm = ref.watch(addMenuViewModelProvider.notifier);
-    return Stack(alignment: Alignment.bottomRight, children: [
-      Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 21.0),
-        child: SizedBox(
-          width: double.infinity,
-          height: 260,
-          child: GestureDetector(
-            onTap: () => vm.setImage(),
-            child: ClipRRect(
-                borderRadius: const BorderRadius.all(Radius.circular(16)),
-                child: state.imageFile != null
-                    ? Image(
-                        image: FileImage(state.imageFile!), fit: BoxFit.cover)
-                    : const Image(
-                        image: AssetImage("assets/images/noImage.png"),
-                        fit: BoxFit.cover)),
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 21.0),
+          child: SizedBox(
+            width: double.infinity,
+            height: 260,
+            child: GestureDetector(
+              onTap: () => vm.setImage(),
+              child: ClipRRect(
+                  borderRadius: const BorderRadius.all(Radius.circular(6)),
+                  child: state.imageFile != null
+                      ? Image(
+                          image: FileImage(state.imageFile!), fit: BoxFit.cover)
+                      : const Image(
+                          image: AssetImage(
+                              "assets/images/add_image_background.png"),
+                          fit: BoxFit.fill)),
+            ),
           ),
         ),
-      ),
-      GestureDetector(
-        child: SvgPicture.asset('assets/icons/change.svg'),
-        onTap: () => vm.setImage(),
-      ),
-    ]);
+        Visibility(
+          visible: state.imageFile == null,
+          child: Column(
+            children: [
+              SvgPicture.asset(
+                'assets/icons/add_image.svg',
+                width: 38,
+                height: 38,
+              ),
+              const ColumnPadding(height: 8),
+              const Text(
+                '写真を追加',
+                style: TextStyle(fontSize: 12, color: Color(green)),
+              )
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
