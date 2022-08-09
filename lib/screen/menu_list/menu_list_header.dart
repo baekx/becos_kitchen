8003eb5ad1c92c5f3a7c324fd284fbc24476aeb8
seo <seungbaek.seo@spacemarket.co.jp>
2022-08-09 12:08:19 +0900

    ホームアイコン並び替え

--- lib/screen/menu_list/menu_list_header.dart
+++ lib/screen/menu_list/menu_list_header.dart
@@ -1,4 +1,5 @@
 import 'package:becos_kitchen/screen/common/circle_user_avatar.dart';
+import 'package:becos_kitchen/screen/common/row_padding.dart';
 import 'package:becos_kitchen/view_model/menu_list_vm.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -11,50 +12,69 @@ class MenuListHeader extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final vm = ref.watch(menuListViewModelProvider.notifier);
     return AppBar(
+      automaticallyImplyLeading: false,
       toolbarHeight: 80,
       title: Row(
         children: [
-          Expanded(
-            child: Container(
-              width: 280,
-              height: 48,
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(5)),
-              child: Center(
-                child: TextField(
-                  decoration: InputDecoration(
-                      isDense: true,
-                      isCollapsed: true,
-                      contentPadding: const EdgeInsets.symmetric(
-                          vertical: 12.0, horizontal: 16.0),
-                      border: InputBorder.none,
-                      suffixIcon: IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.search),
-                          iconSize: 24),
-                      hintText: "今日はなに食べよう？"),
-                ),
-              ),
-            ),
-          ),
           IconButton(
             onPressed: () {},
             icon: CircleUserAvatar(size: 40, user: vm.getUser()),
           ),
+          const Expanded(child: Center(child: Text('ホーム'))),
+          // Expanded(
+          //   child: Container(
+          //     width: 280,
+          //     height: 48,
+          //     decoration: BoxDecoration(
+          //         color: Colors.white, borderRadius: BorderRadius.circular(5)),
+          //     child: Center(
+          //       child: TextField(
+          //         decoration: InputDecoration(
+          //             isDense: true,
+          //             isCollapsed: true,
+          //             contentPadding: const EdgeInsets.symmetric(
+          //                 vertical: 12.0, horizontal: 16.0),
+          //             border: InputBorder.none,
+          //             suffixIcon: IconButton(
+          //                 onPressed: () {},
+          //                 icon: const Icon(Icons.search),
+          //                 iconSize: 24),
+          //             hintText: "今日はなに食べよう？"),
+          //       ),
+          //     ),
+          //   ),
+          // ),
           IconButton(
-              onPressed: () {},
-              icon: SvgPicture.asset(
-                'assets/icons/filter.svg',
-                width: 16,
-                height: 16,
-              )),
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              'assets/icons/search.svg',
+              width: 16,
+              height: 16,
+            ),
+            padding: EdgeInsets.zero,
+            constraints: BoxConstraints(),
+          ),
+          const RowPadding(width: 16),
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              'assets/icons/filter.svg',
+              width: 16,
+              height: 16,
+            ),
+            padding: EdgeInsets.zero,
+            constraints: BoxConstraints(),
+          ),
+          const RowPadding(width: 16),
           IconButton(
             onPressed: () {},
             icon: SvgPicture.asset(
               'assets/icons/sort.svg',
-              width: 16,
-              height: 16,
+              width: 14,
+              height: 14,
             ),
+            padding: EdgeInsets.zero,
+            constraints: BoxConstraints(),
           ),
         ],
       ),
