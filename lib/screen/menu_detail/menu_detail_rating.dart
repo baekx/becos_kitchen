c656f5af6751298b4be45703dcd87d7bf80a1bad
seo <seungbaek.seo@spacemarket.co.jp>
2022-09-04 18:32:34 +0900

    お気に入り変更ダイアログ追加

--- lib/screen/menu_detail/menu_detail_rating.dart
+++ lib/screen/menu_detail/menu_detail_rating.dart
@@ -2,6 +2,7 @@ import 'package:becos_kitchen/common/material_color.dart';
 import 'package:becos_kitchen/model/person.dart';
 import 'package:becos_kitchen/screen/common/circle_user_avatar.dart';
 import 'package:becos_kitchen/screen/common/row_padding.dart';
+import 'package:becos_kitchen/screen/menu_detail/rate_change_dialog.dart';
 import 'package:becos_kitchen/view_model/menu_detail_vm.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
@@ -14,28 +15,37 @@ class MenuDetailRating extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final menu = ref.watch(menuDetailViewModelProvider);
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        CircleUserAvatar(
-          size: 40,
-          icon: person.imagePath,
-        ),
-        const RowPadding(width: 8),
-        RatingBar.builder(
-          itemBuilder: (context, _) => const Icon(
-            Icons.star,
-            color: Color(starYellow),
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+            context: context,
+            builder: (_) {
+              return RateChangeDialog();
+            });
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircleUserAvatar(
+            size: 40,
+            icon: person.imagePath,
           ),
-          itemCount: 5,
-          itemSize: 24,
-          initialRating: person.isBaek
-              ? menu.rateBaek.toDouble()
-              : menu.rateAkane.toDouble(),
-          ignoreGestures: true,
-          onRatingUpdate: (_) {},
-        )
-      ],
+          const RowPadding(width: 8),
+          RatingBar.builder(
+            itemBuilder: (context, _) => const Icon(
+              Icons.star,
+              color: Color(starYellow),
+            ),
+            itemCount: 5,
+            itemSize: 24,
+            initialRating: person.isBaek
+                ? menu.rateBaek.toDouble()
+                : menu.rateAkane.toDouble(),
+            ignoreGestures: true,
+            onRatingUpdate: (_) {},
+          )
+        ],
+      ),
     );
   }
 }
---
+++ lib/screen/menu_detail/rate_change_dialog.dart
@@ -0,0 +1,85 @@
+import 'package:becos_kitchen/common/material_color.dart';
+import 'package:becos_kitchen/model/person.dart';
+import 'package:becos_kitchen/screen/common/circle_user_avatar.dart';
+import 'package:becos_kitchen/screen/common/column_padding.dart';
+import 'package:becos_kitchen/screen/common/row_padding.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
+class RateChangeDialog extends StatelessWidget {
+  const RateChangeDialog({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      content: Column(mainAxisSize: MainAxisSize.min, children: [
+        const ColumnPadding(height: 16),
+        const Text(
+          'お気に入り度を変更',
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,
+        ),
+        const ColumnPadding(height: 16),
+        CircleUserAvatar(
+          size: 40,
+          icon: Person.baek.imagePath,
+        ),
+        const ColumnPadding(height: 16),
+        RatingBar.builder(
+          itemBuilder: (context, _) => const Icon(
+            Icons.star,
+            color: Colors.amber,
+          ),
+          onRatingUpdate: (_) {},
+          itemPadding: const EdgeInsets.symmetric(horizontal: 8.0),
+          initialRating: 3.0,
+        ),
+        const ColumnPadding(height: 16),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              width: 120,
+              child: ElevatedButton(
+                style:
+                    ElevatedButton.styleFrom(primary: const Color(textColor)),
+                // onPressed: onPressedEnd,
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text(
+                  'キャンセル',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            const RowPadding(width: 6),
+            SizedBox(
+              width: 120,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(primary: const Color(green)),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text(
+                  '変更',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            const ColumnPadding(height: 16),
+          ],
+        )
+      ]),
+    );
+  }
+}
